17c62377690aaefb6d13419e6d87d742bc815be6
Yiwen Zhang <113214994+VikingCrusader@users.noreply.github.com>
2025-07-09 08:16:17 +0200

    Add files via upload

---
+++ myutils/calculator.java
@@ -0,0 +1,12 @@
+package myutils;
+
+public class calculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
---
+++ myutils/mainApp.java
@@ -0,0 +1,9 @@
+package myutils;
+import myutils.calculator;
+
+public class mainApp {
+    public static void main(String[] args) {
+        System.out.println(calculator.add(1,2 ));
+        System.out.println(calculator.subtract(5,2));
+    }
+}
